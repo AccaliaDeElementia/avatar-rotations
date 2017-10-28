42eb12206a5a4ac83356f9327900d2f7cb0c47ac
Accalia Elementia <accalia@elementia.me>
2017-10-28 12:08:29 +0000

    add standard

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const {validExtensions, getImages, sendFile} = require('./image');
+const { validExtensions, getImages, sendFile } = require('./image')
 
 const serverOpts = {
   port: process.env.PORT || 8888,
   ip: process.env.IP || '127.0.0.1',
   baseDir: process.env.AVATARS_BASEDIR || 'Images'
-};
+}
 
-
-
-const hour = 60 * 60 * 1000;
-const day = 24 * hour;
+const hour = 60 * 60 * 1000
+const day = 24 * hour
 const choosers = {
   first: (choices) => choices[0],
   latest: (choices) => choices[choices.length - 1],
   random: (choices) => choices[Math.floor(Math.random() * choices.length)],
   sequence: (choices) => choices[Math.floor(Date.now() / hour) % choices.length],
   daily: (choices) => choices[Math.floor(Date.now() / day) % choices.length]
-};
+}
 
 const stripExtension = (after) => (req, res, next, value) => {
-  const parts = value.split('.');
+  const parts = value.split('.')
   if (validExtensions.some((ext) => ext === parts[parts.length - 1])) {
-    parts.pop();
+    parts.pop()
   }
-  after(req, res, parts.join('.'));
-  next();
-};
+  after(req, res, parts.join('.'))
+  next()
+}
 
 app.param('folder', stripExtension((req, res, folder) => {
-  req.AVATARS_FOLDER = `${serverOpts.baseDir}/${folder.replace(/:/g, '/')}`;
-}));
+  req.AVATARS_FOLDER = `${serverOpts.baseDir}/${folder.replace(/:/g, '/')}`
+}))
 
-app.param('chooser', stripExtension((req, res, chooser) => req.AVATARS_CHOOSER = chooser));
+app.param('chooser', stripExtension((req, res, chooser) => { req.AVATARS_CHOOSER = chooser }))
 
-app.param('width', stripExtension((req, res, width) => req.AVATARS_WIDTH = Number.parseInt(width, 10)));
+app.param('width', stripExtension((req, res, width) => { req.AVATARS_WIDTH = Number.parseInt(width, 10) }))
 
 app.get(/^\/(favicon[.]ico)?$/, (req, res) => sendFile('favicon.ico', undefined, res))
 
@@ -45,9 +43,9 @@ const enforceAvatarOptions = (req, res, next) => {
     folder: req.AVATARS_FOLDER,
     chooser: choosers[req.AVATARS_CHOOSER] || choosers.random,
     width: req.AVATARS_WIDTH >= 10 && req.AVATARS_WIDTH <= 1000 ? req.AVATARS_WIDTH : undefined
-  };
-  next();
-};
+  }
+  next()
+}
 
 app.get('/:folder/:chooser?/:width?', enforceAvatarOptions, (req, res) => {
   getImages(req.AVATARS_OPTS.folder)
@@ -55,10 +53,9 @@ app.get('/:folder/:chooser?/:width?', enforceAvatarOptions, (req, res) => {
     .then(image => `${req.AVATARS_OPTS.folder}/${image}`)
     .then(image => sendFile(image, req.AVATARS_OPTS.width, res))
     .catch((err) => {
-      console.error(err);
-      res.status(400).send('error');
-    });
-});
+      console.error(err)
+      res.status(400).send('error')
+    })
+})
 
-
-app.listen(serverOpts.port);
+app.listen(serverOpts.port)
