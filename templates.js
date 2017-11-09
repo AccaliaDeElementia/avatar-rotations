3dad714f351d87396b74d37864b35fc53c9cb088
Accalia Elementia <accalia@elementia.me>
2017-11-09 14:52:27 +0000

    remove unsed file

--- templates.js
+++
@@ -1,9 +0,0 @@
-const handlebars = require('handlebars')
-const { readFile } = require('fs')
-
-exports.sendTemplate = (context) => new Promise((resolve, reject) => {
-    readFile(context.template, {encoding:'utf8'}, (err, data) => err ? reject(err) : resolve(data))
-  })
-  .then(template => {
-      context.res.send(handlebars.compile(template)(context.data))
-  })
