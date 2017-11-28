daa1743e8e792c90a73d38952a4d915d430d0075
Accalia Elementia <accalia@elementia.me>
2017-11-28 12:29:04 +0000

    resolve codeclimate issue
    also add pagination to bottom of listing as well as top

--- routes/listing.js
+++ routes/listing.js
@@ -2,28 +2,31 @@ const express = require('express')
 const { getListing } = require('./api/listing')
 const { handleError, ExpressRedirectError } = require('../utils/errors')
 
+const shimData = (data, page, app, req) => {
+  data.size = req.params.size || '300'
+  console.log(data.pages.current, page)
+  if (data.pages.current !== page) {
+    const dest = [app.path(), `size-${data.size}`, `${req.params['0']}?page=${data.pages.current}`]
+    throw new ExpressRedirectError(dest.join('/'))
+  }
+  return data
+}
+
 module.exports = serverOpts => {
   const error404 = (req, res) => handleError(serverOpts, res, new ExpressRedirectError('Nobody here but is chickens.', 404))
   const app = express()
   app.get(['/', '/size-', '/size-:size'], error404)
   app.get('/size-:size/*', (req, res) => {
     const page = parseInt(req.query.page || '1', 10)
-    return getListing({
+    const opts = {
       webRoot: app.path(),
       basePath: serverOpts.baseDir,
       directory: req.params['0'],
       page: page,
       pageSize: 50
-    })
-      .then(data => {
-        data.size = req.params.size || '300'
-        console.log(data.pages.current, page)
-        if (data.pages.current !== page) {
-          const dest = [app.path(), `size-${data.size}`, `${req.params['0']}?page=${data.pages.current}`]
-          throw new ExpressRedirectError(dest.join('/'))
-        }
-        return data
-      })
+    }
+    return getListing(opts)
+      .then(data => shimData(data, page, app, req))
       .then(data => res.render('listing', data))
       .catch(e => handleError(serverOpts, res, e))
   })
