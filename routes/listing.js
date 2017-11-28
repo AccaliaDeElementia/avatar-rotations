11ae448b67951ef917a4a158e0b72473b0e84c51
Accalia Elementia <accalia@elementia.me>
2017-11-28 12:17:48 +0000

    force page parameter to be in range

--- routes/listing.js
+++ routes/listing.js
@@ -9,14 +9,19 @@ module.exports = serverOpts => {
   app.get('/size-:size/*', (req, res) => {
     const page = parseInt(req.query.page || '1', 10)
     return getListing({
-      webRoot: app.path(),
-      basePath: serverOpts.baseDir,
-      directory: req.params['0'],
-      page: page,
-      pageSize: 50
-    })
+        webRoot: app.path(),
+        basePath: serverOpts.baseDir,
+        directory: req.params['0'],
+        page: page,
+        pageSize: 50
+      })
       .then(data => {
         data.size = req.params.size || '300'
+        console.log(data.pages.current, page)
+        if (data.pages.current !== page) {
+          const dest = [app.path(), `size-${data.size}`, `${req.params['0']}?page=${data.pages.current}`]
+          throw new ExpressRedirectError(dest.join('/'))
+        }
         return data
       })
       .then(data => res.render('listing', data))
